9383c4a1ef72422e011b90444fa299e08f88976f
Natalie Cygan <nataliemcygan@gmail.com>
2021-08-11 16:32:58 -0700

    Processing nev to csv for editing comments

--- sandbox.py
+++ sandbox.py
@@ -6,9 +6,16 @@ import scipy.io as sio
 import utils as util
 
 DATA_DIR = "mat_files"
-
-nev_path = os.path.join(DATA_DIR, "OE_recording_Blackrock013_NEV.mat")
-nsx_path = os.path.join(DATA_DIR, "OE_recording_Blackrock013_NS6.mat")
+NEV_FILENAME = "OE_recording_Blackrock013_NEV.mat"
+NSX_FILENAME = "OE_recording_Blackrock013_NS6.mat"
+nev_path = os.path.join(DATA_DIR, NEV_FILENAME)
+nsx_path = os.path.join(DATA_DIR, NSX_FILENAME)
 
 nev = sio.loadmat(nev_path)
 nsx = sio.loadmat(nsx_path)
+
+comments = util.get_comments(nev)
+data = util.get_data(nsx)
+
+comment_path = "OE_recording_Blackrock" + NEV_FILENAME[-11:-8] + "_comments.csv"
+util.save_comments_to_csv(comments, comment_path)
--- utils.py
+++ utils.py
@@ -1,18 +1,27 @@
 import numpy as np
 import scipy.io as sio
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import pandas as pd
+import glob
+
+
+BLACKROCK_DATA_DIR = 'blackrock_data'
 
 def get_comments(nev):
     """
     Returns a dictionary containing three abributes that define the comments 
     from an nev .mat object:
         - 'text': (N, ) array containing the raw text of the comments
-        - 'start': (1, N) array containing the start times of the comments
-        - 'end': (1, N) array containing the end times of the comments
+        - 'start': (N, ) array containing the start times of the comments
+        - 'end': (N, ) array containing the end times of the comments
     """
     comments = {}
     comments['text'] = nev['NEV']['Data'][0][0]['Comments'][0][0]['Text'][0][0]
-    comments['start'] = nev['NEV']['Data'][0][0]['Comments'][0][0]['TimeStampStarted'][0][0]
-    comments['end'] = nev['NEV']['Data'][0][0]['Comments'][0][0]['TimeStamp'][0][0]
+    comments['start'] = \
+        nev['NEV']['Data'][0][0]['Comments'][0][0]['TimeStampStarted'][0][0][0]
+    comments['end'] = \
+        nev['NEV']['Data'][0][0]['Comments'][0][0]['TimeStamp'][0][0][0]
 
     return comments
 
@@ -23,3 +32,26 @@ def get_data(nsx, mode='NS6'):
     """
     data = nsx[mode]['Data'][0][0]
     return data
+
+def plot_data(data, comments):
+    # fig = go.Figure()
+    # fig.add_trace(go.Scatter(x=np.arange(data.shape[1]), y=data,
+                    # mode='lines'))
+    # fig.show()
+
+    plt.plot(np.arange(data.shape[1]), data[0,:], label="EOG Data")
+
+def save_comments_to_csv(comments, pathname):
+    df = pd.DataFrame(comments)
+    df.to_csv(pathname, index=False)
+
+def process_all_nev_to_csv(data_dir='mat_files'):
+    files = glob.glob(data_dir + '/*NEV.mat', recursive=True)
+
+    for f in files:
+        num = f[-11:-8]
+        nev = sio.loadmat(nev_path)
+        comments = get_comments(nev)
+
+        comment_path = "OE_recording_Blackrock" + num + "_comments.csv"
+        save_comments_to_csv(comments, comment_path)
