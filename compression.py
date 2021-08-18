797bddf5b75d8662c67a98ea1120add5d1e1caa1
Natalie Cygan <nataliemcygan@gmail.com>
2021-08-17 23:59:14 -0700

    Use tobytes instead of savetxt

--- compression.py
+++ compression.py
@@ -13,19 +13,28 @@ def compression_experiment(recs, keys):
 
     compression_results = {}
 
-    min_length = get_min_length(recs, keys)
+    min_length = util.get_min_length(recs, keys)
 
     for rec_id in keys:
         for cond in keys[rec_id]:
-            data_slice = get_condition_slice(recs[rec_id]['cond'], cond,
-                                                recs[rec_id]['data']) 
+            data_slice = util.get_condition_slice(recs[rec_id]['cond'], cond,
+                    recs[rec_id]['data'])[:min_length]
+
+            data_bytes = data_slice.tobytes()
     
-    # Get file size (in bytes) without compression 
-    np.savetxt('slice.npy', data_slice)
-    size = Path('slice.npy').stat().st_size
+            # Get file size (in bytes) without compression 
+            np.savetxt('slice.npy', data_slice)
+            size_raw = Path('slice.npy').stat().st_size
 
-    # Get file size (in bytes) with compression 
-    np.savetxt('slice.gz', data_slice)
-    size = Path('slice.gz').stat().st_size
+            # Get file size (in bytes) with compression 
+            np.savetxt('slice.gz', data_slice)
+            size_gz = Path('slice.gz').stat().st_size
 
+            ratio = size_gz / size_raw
 
+            print('<' + rec_id + ': ' + cond + '>: raw size: ' + str(size_raw) + \
+                    ' compressed size: ' + str(size_gz) + ' ratio: ' + \
+                    str(ratio))
+
+            if rec_id not in compression_results:
+                compression_results['rec_id'] = {}
