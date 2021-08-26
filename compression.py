506fbde5dd2a0a953a7f134080b8673be081a1f1
Natalie Cygan <nataliemcygan@gmail.com>
2021-08-25 19:31:19 -0700

    More continuous compression analysis

--- compression.py
+++ compression.py
@@ -7,6 +7,7 @@ import pdb
 
 import numpy as np
 import plotly.graph_objects as go
+from tqdm import tqdm
 
 import utils as util
 
@@ -64,3 +65,43 @@ def plot_compression_results(result):
                         yaxis_title='Recording condition')
     fig.show()
         
+
+def compress_recording(data, window_size=90000):
+
+    comp_ratios = []
+
+    num_slices = data.shape[0] // window_size
+
+    for i in tqdm(range(num_slices)):
+
+        # Snip data slice, convert to bytes
+        data_slice = data[i * window_size: i * window_size + window_size]
+        data_slice = data_slice.astype('float32')
+        data_bytes = data_slice.tobytes()
+
+        # Compress
+        data_bytes_compressed = gzip.compress(data_bytes)
+
+        # Get raw size, ratio size
+        size_raw = len(data_bytes)
+        size_gz = len(data_bytes_compressed)
+        ratio = size_gz / size_raw
+
+        comp_ratios.append(ratio)
+    
+    return comp_ratios
+
+def plot_ratios(ratios):
+
+    np_ratios = np.asarray(ratios)
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=np.arange(np_ratios.shape[0]), y=np_ratios, 
+        name='Compression ratio (gz size / raw size)',
+        mode='markers'))
+
+    fig.update_layout(title='Compressibility over EOG recording',
+                        yaxis_title='Compressibility ratio (gz size / raw size)',
+                        xaxis_title='Recording samples')
+    fig.show()
+
